4704c36ba6b89e496ecd2ea56d152ccedc9baa29
Venkata Dasu <vdasu@mail.usf.edu>
2019-09-24 21:33:17 -0400

    Add project files.

---
+++ Professor.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using InClassExercise_Classes;
+
+namespace Professor
+{
+    class Professor : InClassExercise_Classes.Person
+    {
+        public string Employee_ID;
+
+        public Professor()
+        {
+            Employee_ID = "P1234";
+        }
+
+        public Professor(string PNumber, string Isal, string ftName, string ltName) : base(ltName)
+        {
+            Employee_ID = PNumber;
+        }
+
+        new public string getName()
+        {
+            return Employee_ID + " " + base.getName();
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace InClassExercise_Classes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+
+
+
+            
+        }
+    }
+}
